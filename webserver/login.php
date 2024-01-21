11ebd7d94058b483bcb5c87ea09b248a327ee2a0
roslanskyc <roslanskyc@gmail.com>
2024-01-21 16:37:32 -0600

    added auth

--- webserver/login.php
+++ webserver/login.php
@@ -2,7 +2,7 @@
 
 //include "db_conn.php";
 
-if(isset($_POST['uname']) && isset($_POST['password'])){
+if(isset($_POST['username']) && isset($_POST['password'])){
 
     function validate($data){
         $data = trim($data);
@@ -17,12 +17,20 @@ $pass = validate($_POST['password']);
 
 if(empty($username)){
     header ("Location: index.html?erro=Username is required");
-    exit();
+    
 }
 else if(empty($pass)){
     header ("Location: index.html?erro=Password is required");
-    exit();
+    
 }
+
+header("Authorization: Basic " . base64_encode($_POST['username'] . ":" . $_POST['password']));
+if($_POST['username'] == "CEO"){
+    header("Account-Type: CEO");
+} else {
+    header("Account-Type: User");
+}
+include("landing/land.php");
 
 
 
