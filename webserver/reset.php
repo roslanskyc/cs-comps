e64117c37468839f15a4bcf0a39fa9da5c9de246
Ariana Borlak <borlaka@carleton.edu>
2024-02-07 11:14:04 -0600

    email draft

---
+++ webserver/reset.php
@@ -0,0 +1,71 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+function generate_6_digit_OTP() {
+    $OTP = "";
+    for($i = 1; $i <=6; $i++) {
+            $int = rand(0,9);
+            $str = "$int";
+            $OTP .= $str;
+    }
+	return $OTP;
+}
+
+
+$email = $_POST["email"];
+
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+$mail = new PHPMailer(true);
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	echo("$email is a valid email address\n");
+
+	$reset_code = generate_6_digit_OTP();
+	echo($reset_code);
+
+	try {
+	//Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->Port = 587;									//TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+	$mail->SMTPAuth = true;                                   //Enable SMTP authentication
+
+	//Recipients
+    $mail->setFrom('zereaux.day@gmail.com', 'Zereaux Day');
+    $mail->addAddress($email);
+
+    //Attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg'); 	  //Add attachments
+
+    //Content
+    $mail->isHTML(false);                                  //Set email format to HTML
+    $mail->Subject = 'Reset Code';
+    $mail->Body = "Your one-time password reset code is:\n$reset_code";
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+
+	} catch (Exception $e) {
+    	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+	}
+
+	// $msg = "Your one-time password reset code is:\n$reset_code";
+	// mail($email,"Reset Code",$msg);
+	// echo("Mail should've been sent");
+
+} else {
+	echo("$email is not a valid email address");
+}
+
+// header("Location:" . "reset_code.html");
+
+?>
