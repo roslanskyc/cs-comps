439634e4be2aa442e2cde27a0d41924f5f37370c
roslanskyc <roslanskyc@gmail.com>
2024-02-14 13:13:19 -0600

    login form

--- implementation1/login.php
+++ implementation1/login.php
@@ -1,6 +1,15 @@
 <?php
 
-mail($_POST['email'], "hello", "hello");
-include("index.html");
+$user = $_POST['username'];
+$pass = hash('sha256', $_POST['pass']);
+$db = pg_connect("host=localhost dbname=auth user=postgres password=postgres");
+
+$result = pg_prepare($db, "logcheck", "SELECT * FROM credentials WHERE username = $1 AND password = $2");
+
+$result = pg_execute($bd, "logcheck", array($user, $pass));
+
+echo $result;
+
+
 
 ?>
